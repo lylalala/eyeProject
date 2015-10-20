a952f344fbf5bde339920cc7b295deabc9b65572
LiuYang <liuyang070424@163.com>
2015-10-20 10:45:48 +0800

    the solution to modify the file by "awk"

--- project_Modify.sh
+++ project_Modify.sh
@@ -2,9 +2,14 @@
 
 #add the boost to this project
 #modify ../current_head/src/CMakeList.txt
-path="/usr/local/Library/boost"
-cd current_head
-cd src
+#path="/usr/local/Library/boost"
+#cd current_head
+#cd src
 #sed -i "" 'PROJECT(JZP_EYE_TRACKING)/a\set(BOOST_ROOT ${path})' CMakeLists.txt
 #sed -i  "" "s:PROJECT(JZP_EYE_TRACKING):PROJECT(JZP_EYE_TRACKING)\nset(BOOST_ROOT ${path}):g" CMakeLists.txt
-sed -i  "" ":PROJECT(JZP_EYE_TRACKING):a\set(BOOST_ROOT ${path})" CMakeLists.txt
+#sed -i  "" ":PROJECT(JZP_EYE_TRACKING):a\set(BOOST_ROOT ${path})" CMakeLists.txt
+
+
+awk 'FNR<4' test.txt > newfile.txt
+echo -e "\t" >>newfile.txt
+awk 'FNR<5' test.txt >> newfile.txt
