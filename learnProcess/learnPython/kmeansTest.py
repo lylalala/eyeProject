79ba6fcd073f6755796c66b923f82d685714a2b8
LiuYang <liuyang070424@163.com>
2015-10-29 20:00:08 +0800

    1.2 update the means

--- learnProcess/learnPython/kmeansTest.py
+++ learnProcess/learnPython/kmeansTest.py
@@ -16,7 +16,7 @@ def fillHoleInBinary(Img,binaryImg):
 
 if __name__=="__main__":
     efficient=np.array([1,0.2,0.2,0.9])
-    Img=cv2.imread('/Users/ly/Code/picAfter/eye48_2.jpg')
+    Img=cv2.imread('/Users/ly/Code/picAfter/eye12_1.jpg')
     cv2.imshow('Img',Img)
     cv2.waitKey()
     grayImg=cv2.cvtColor(Img,cv2.COLOR_BGR2GRAY)
@@ -26,28 +26,16 @@ if __name__=="__main__":
     noHoleImg=fillHoleInBinary(Img,binaryImg)
     cv2.imshow('noHoleImg',noHoleImg)
     cv2.waitKey()
-    l1=noHoleImg[:,:,0]*efficient[0]
-    l2=noHoleImg[:,:,1]*efficient[1]
-    l3=noHoleImg[:,:,2]*efficient[2]
-    l1=np.reshape(l1.T,(l1.shape[0]*l1.shape[1],1))
-    l2=np.reshape(l2.T,(l2.shape[0]*l2.shape[1],1))
-    l3=np.reshape(l3.T,(l3.shape[0]*l3.shape[1],1))
-    x=np.arange(Img.shape[0])
-    y=np.arange(Img.shape[1])
-    x=np.tile(x,Img.shape[1])
-    y=y.repeat(Img.shape[0])
-    x=np.reshape(x,(x.shape[0],1))
-    y=np.reshape(y,(y.shape[0],1))
-    data=np.hstack((l1,l2,l3,x,y))
+    l1=np.reshape(noHoleImg[:,:,0].T,(Img.shape[0]*Img.shape[1],1))
+    l2=np.reshape(noHoleImg[:,:,1].T,(Img.shape[0]*Img.shape[1],1))
+    l3=np.reshape(noHoleImg[:,:,2].T,(Img.shape[0]*Img.shape[1],1))
+    x=np.reshape(np.tile(np.arange(Img.shape[0]),Img.shape[1]),(l1.shape[0],1))
+    y=np.reshape(np.arange(Img.shape[1]).repeat(Img.shape[0]),(l1.shape[0],1))
+    data=np.hstack((l1*efficient[0],l2*efficient[1],l3*efficient[2],x*efficient[3],y*efficient[3]))
+    #the first time to operate kmeans
     kmeans=KMeans(n_clusters=2,random_state=0).fit(data)
     labels=kmeans.predict(data)
-    #labels=np.reshape(labels,(Img.shape[1],Img.shape[0])).T
-    #print labels.shape
-    #labels=labels*255
-    #labels=255-labels
-    #labels=np.uint8(labels)
-    #cv2.imshow('kmenas',labels)
-    #cv2.waitKey()
+    #find the XXX
     if np.mean(data[np.where(labels==1),0:3])>np.mean(data[np.where(labels==0),0:3]):
         ti=0
     else:
@@ -55,29 +43,16 @@ if __name__=="__main__":
     labels[np.where(labels!=ti)]=2
     newdata=data[np.where(labels==ti)]
     newdataIndex=np.where(labels==ti)[0]
-    #newdataIndex=newdataIndex[0]
+    #the second time to operate kmeans
     kmeans=KMeans(n_clusters=2,random_state=0).fit(newdata)
     newlabels=kmeans.predict(newdata)
-    print '-'*40
     if np.mean(newdata[np.where(newlabels==1),0:3])>np.mean(newdata[np.where(newlabels==0),0:3]):
-        temp=np.where(newlabels==1)
-        temp=temp[0]
-        labels[newdataIndex[temp]]=0
-        temp=np.where(newlabels==0)
-        temp=temp[0]
-        labels[newdataIndex[temp]]=1
+        labels[newdataIndex[np.where(newlabels==1)[0]]]=0
+        labels[newdataIndex[np.where(newlabels==0)[0]]]=1
     else:
-        temp=np.where(newlabels==0)
-        temp=temp[0]
-        labels[newdataIndex[temp]]=0
-        temp=np.where(newlabels==1)
-        temp=temp[0]
-        labels[newdataIndex[temp]]=1
-    labels=np.reshape(labels,(Img.shape[1],Img.shape[0])).T
-    print labels.shape
-    labels=labels*127
-    labels=255-labels
-    labels=np.uint8(labels)
+        labels[newdataIndex[np.where(newlabels==0)[0]]]=0
+        labels[newdataIndex[np.where(newlabels==1)[0]]]=1
+    labels=np.uint8(255-np.reshape(labels,(Img.shape[1],Img.shape[0])).T*127)
     cv2.imshow('kmenas',labels)
     cv2.waitKey()
 
