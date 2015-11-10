9eb3cd510c6c265e9b36473b7c72009d7cd4ad90
LiuYang <liuyang070424@163.com>
2015-11-10 21:08:30 +0800

    3. improve refine the code --(2) some modify: once KMean/ cout the 'covstackdiffmax'/ find the top-10 point and select the proper one

---
+++ learnProcess/learnPython/eye_fillhole_kmeans.py
@@ -0,0 +1,6 @@
+import cv2
+import numpy as np
+
+eye
+
+#eye_to_fillhole
---
+++ learnProcess/learnPython/my.sh
@@ -0,0 +1,13 @@
+#/bin/sh
+cd /Users/ly/MyGithub/eyeProject/learnProcess/learnPython
+cd eye
+rm -rf *
+cd ..
+cd fillhole
+rm -rf *
+cd ..
+cd kmeans
+rm -rf *
+cd ..
+cd max
+rm -rf *
